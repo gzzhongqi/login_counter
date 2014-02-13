abe740ae401d19c866c7e1b600ea7099b13892bc
TZ2016 <tianhao.zhang.2016@gmail.com>
2014-02-12 21:53:46 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Counter::Application.config.secret_key_base = 'a6100f48069646a3210246c1ee72d90f7942b41e01bb16b98b4740ea0416c8230b2386c5eb45f20d54f3ac560439e7792b891c5023bcc44444f970208cd0967b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Counter::Application.config.session_store :cookie_store, key: '_counter_session'
